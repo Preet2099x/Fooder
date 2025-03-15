9cd15c5eb506d8e2ff4f6bca2028346d06893b2c
Preet2099x <preet2099x@gmail.com>
2025-03-15 08:59:36 +0530

    Added basic structure

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Navbar from '../components/Navbar'
+import Footer from '../components/Footer'
+
+function HomePage() {
+  return (
+    <>
+      <Navbar />
+      <div>HomePage</div>
+      <Footer />
+    </>
+  )
+}
+
+export default HomePage
